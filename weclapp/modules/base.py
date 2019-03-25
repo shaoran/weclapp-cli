42f3ef04cdb1cba50c66811fe368baebcfb84433
Shaoran <shaoran@sakuranohana.org>
2019-03-25 01:53:15 +0100

    adding more function to the base module class

--- weclapp/modules/base.py
+++ weclapp/modules/base.py
@@ -3,8 +3,16 @@ class BaseModule(object):
     The base module
     """
 
-    cmdline = "base"
+    name = 'base'
+    cmdline_opts = {}
+
+    def __init__(self, app, namespace):
+        self.app = app
+        self.namespace = namespace
 
     @staticmethod
     def init_argparser(parser):
         pass
+
+    def run(self, namespace):
+        raise Exception('The run method has to be overriden')
