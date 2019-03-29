d2ae19c07955514fc5ccff146948051cebe64d85
Shaoran <shaoran@sakuranohana.org>
2019-03-30 00:02:16 +0100

    adding cli arguments and epilog in the project module

--- weclapp/modules/project.py
+++ weclapp/modules/project.py
@@ -1,19 +1,38 @@
+from argparse import RawTextHelpFormatter
+
 from .base import BaseModule
 from ..models import WeclappProject
 
 basehelp = 'Print information about the projects and tasks'
+epilog = """
+If you use --projects-only or --projects-with-tasks then time
+records are not fetched.
+
+If you want to display all time records, then use --last all
+"""
 
 class ProjectModule(BaseModule):
     name = 'projects'
     cmdline_opts = {
         'help': basehelp,
         'description': basehelp,
+        'epilog': epilog,
+        'formatter_class': RawTextHelpFormatter,
     }
 
     @staticmethod
     def init_argparser(parser):
-        parser.add_argument('-j', '--json', action='store_true', default=False, dest='json',
-                help='Print the results as JSON')
+        parser.add_argument('-p', '--projects-only', action='store_true', default=False, dest='projects_only',
+                help='Show only projects')
+
+        parser.add_argument('-t', '--projects-with-tasks', action='store_true', default=False, dest='projects_and_tasks',
+                help='Show only projects and tasks.')
+
+        parser.add_argument('-l', '--last', action='store', default=100, dest='last', metavar='N',
+                help='Show the last N time records.')
+
+        parser.add_argument('-q', '--query', action='store', default='', dest='query', metavar='QUERY',
+                help='Filter projects and tasks by query.\nDisplays the projects & tasks that contains the query.')
         parser.set_defaults(module = ProjectModule)
 
 
