3d26a256a9156e4e4dd15bb0903905ed4077fa24
Shaoran <shaoran@sakuranohana.org>
2019-04-02 02:23:10 +0200

    adding parsing for the csv parser

--- weclapp/parser/csvparser.py
+++ weclapp/parser/csvparser.py
@@ -1,4 +1,11 @@
+import logging
+
+from dateutil.parser import parse as date_parse
+
 from .parser import Parser
+from .exceptions import FailedToParse, InvalidParserOptionFormat
+
+log = logging.getLogger("weclapp-cli")
 
 class CSVParser(Parser):
     """
@@ -36,4 +43,47 @@ class CSVParser(Parser):
     The table can have unlimited number of projects, every project must consist of
     two columns, one with the time, another with the description.
     """
-    pass
+    __options__ = [
+        ('sep', ';', 'The CSV separator'),
+        ('timeStart', '09:00', 'The start of the time record'),
+        ('encoding', 'utf-8', 'Default file encoding'),
+    ]
+
+    def parseFile(self, filename):
+        try:
+            with open(filename, encoding=self.options.encoding) as fp:
+                lines = [ x.strip() for x in fp.readlines() ]
+        except:
+            log.debug('Cannot open the file', exc_info=True)
+            raise FailedToParse('Could not open file')
+
+        if len(lines) < 3:
+            raise FailedToParse('Invalid format, header is missing or first data line is missing')
+
+        pinfo = []
+
+        headers = [ lines[0].split(self.options.sep), lines[1].split(self.options.sep) ]
+
+        hl = abs(len(headers[0]) - len(headers[1]))
+        if hl > 1:
+            raise FailedToParse('Invalid format, the header rows do not match, invalid length')
+
+        if hl == 1:
+            if len(headers[0]) > len(headers[1]):
+                idx = 1
+            else:
+                idx = 0
+            headers[idx].append('')
+
+        if len(headers[0]) % 2 == 0:
+            headers[0].append('')
+            headers[1].append('')
+
+        # thanks to https://opensourcehacker.com/2011/02/23/tuplifying-a-list-or-pairs-in-python/
+        headers = [ list(zip(*[h[x::2] for x in (1, 2)])) for h in headers ]
+        headers = [ [ h[0] for h in k ] for k in headers ]
+        headers = list(zip(*headers))
+
+        print(headers)
+
+        return []
